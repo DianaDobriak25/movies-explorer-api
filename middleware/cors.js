883be04ec988a3355b449e0fc1d124eb896e5224
Dobriak Diana <di.dobryak@yandex.ru>
2022-07-21 10:36:45 +0300

    update

--- middleware/cors.js
+++ middleware/cors.js
@@ -11,6 +11,8 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
 
+  console.log(origin);
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', '*');
   }
